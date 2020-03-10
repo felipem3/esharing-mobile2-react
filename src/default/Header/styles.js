b599d36d68e17d92bf59b6ef58a8c8dab7c6cd1f
Felipe Tatizawa <massakitzw@gmail.com>
2020-03-09 23:12:13 -0300

    teste

---
+++ src/default/Header/styles.js
@@ -0,0 +1,25 @@
+import { StyleSheet } from 'react-native';
+
+export default styles = StyleSheet.create({
+  header: {
+    paddingTop: 25,
+    height: 65,
+    flexDirection:"row", 
+    alignItems:"center",
+    justifyContent:"center",
+    backgroundColor:"#1eb2a6"
+  },
+  headerTitle: {
+    fontWeight: "bold",
+    fontSize: 18,
+    color:"#FFF",
+  },
+  fotoDePerfil: { 
+    marginRight:10, 
+    borderRadius:20, 
+    width: 40, 
+    height: 40
+  },
+  foto: { 
+    }
+}); 
